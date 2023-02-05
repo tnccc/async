79ecfcd985cfa9d828ea8778f69c8b20ff1a8485
tnccc <irei.frontend@gmail.com>
2023-02-05 15:22:59 +0900

    async.js作成

--- async.js
+++ async.js
@@ -1,24 +1,19 @@
 import fetch from 'node-fetch';
 
 // 非同期処理をおこなう関数を宣言
-const getGitUsername = () => {
-    return new Promise((resolve, reject) => {
-        const url = 'https://api.github.com/users/tnccc'
+const getGitUsername = async () => {
+  const message = 'GitのユーザーIDは';
+  const url = 'https://api.github.com/users/tnccc'
 
-        // GitHub APIをFetchメソッドで実行
-        fetch(url).then(res => res.json())
-            .then(json => {
-                console.log('これは非同期処理成功時のメッセージです')
-                return resolve(json.login)
-            }).catch(error => {
-                console.error('これは非同期処理失敗時のメッセージです。', error)
-                return reject(null)
-            })
-
+  const json = await fetch(url)
+    .then(res => {
+      console.log('これは非同期処理成功時のメッセージです')
+      return res.json()
+    }).catch(error => {
+      console.error('これは非同期処理失敗時のメッセージです。', error)
     })
-};
 
-const message = 'GitのユーザーIDは'
-getGitUsername().then(username => {
+    const username = json.login;
     console.log(message + username)
-});
+}
+getGitUsername()
